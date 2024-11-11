6334b8912669b8d2349c1160d46132df8bfd0638
caiquin <caique.tavares@fatec.sp.gov.br>
2024-11-11 18:13:28 -0300

    exbindo perfil do usuario(nome) e logout

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -4,33 +4,90 @@ import {
   Nickname,
   Links,
   Container,
+  LogoutButton,
+  LogoutButtonText
 } from "@/assets/styles/profile.styles";
 import { getUserById } from "@/services/users";
 import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getToken } from "@/services/api";
-import { useLocalSearchParams } from "expo-router";
+import { jwtDecode } from "jwt-decode";
+import { Text } from "react-native";
+import { useRouter, Link } from "expo-router";
+
 
 interface User{
   id: string,
   name:string
 }
 
-
-
 export default function ProfileScreen() {
   const [user, setUser] = useState<User | null>(null);
   const [error, setError] = useState("");
+  const router = useRouter();
 
+  const fetchUser = async () => {
+    try {
+      const token = await getToken();
 
+      if (token) {
+        const decodedToken = jwtDecode<{ id: string }>(token);
+
+        if(decodedToken && decodedToken.id){
+          const userId = decodedToken.id;
+          const response = await getUserById(userId);
+          console.log(response.data);
+          setUser(response.data.users);
+        }
+      }
+    } catch (error) {
+      setError("Falha ao recuperar dados do usuário");
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  // const handleLogout = async () => {
+  //   try{
+  //     const token = await getToken();
+  //     console.log("Token antse do logout: ", token);
+
+  //     await AsyncStorage.removeItem("token");
+  //     console.log("Token Removido");
+
+  //     router.replace('/login');
+  //   }catch(error){
+  //     console.error("Erro ao fazer logout: ", error);
+  //   }
+  // }
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.clear(); // Limpa todo o AsyncStorage
+      console.log("Armazenamento limpo"); // Confirmação de limpeza
+  
+      router.replace('/login');
+    } catch (error) {
+      console.error("Erro ao fazer logout:", error);
+    }
+  };
   return (
     <Container>
       <Content>
         <ProfileImage source={require("@/assets/images/profile.png")} />
-        <Nickname>Denilse Veloso</Nickname>
+          {user? (
+            <Nickname>{user.name}</Nickname>
+          ) : (
+            <Nickname>Carregando...</Nickname>
+          )}
         <Links>Seja Premium ⭐</Links>
         <Links>Avaliar Filmes</Links>
       </Content>
+      <LogoutButton onPress={handleLogout}>
+        <LogoutButtonText>Sair</LogoutButtonText>
+      </LogoutButton>
     </Container>
   );
 }
--- assets/styles/profile.styles.ts
+++ assets/styles/profile.styles.ts
@@ -35,3 +35,19 @@ export const Links = styled.Text`
   text-align: left;
   margin-bottom: 10px;
 `;
+
+export const LogoutButton = styled.TouchableOpacity`
+  width: 150px;
+  height: 45px;
+  border-radius: 15px;
+  background-color: #e32b2b;
+  align-items:center;
+  justify-content: center;
+`;
+
+export const LogoutButtonText = styled.Text`
+  color: #262626;
+  font-weight: bold;
+  font-size: 28px;
+  text-align:center;
+`;
